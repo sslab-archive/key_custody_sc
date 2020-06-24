f70799da75e8fae08c07a773b305672891808086
jeyoung117 <capricorn116117@gmail.com>
2020-06-24 20:56:09 +0900

    meeting

--- project_1.sol
+++ project_1.sol
@@ -8,7 +8,7 @@ contract B_project {
         string public_key;  // 프로바이더의 pub_key(이더리움 pub_key와는 다른 개념)
         string name; // 프로바이더의 name
         string base_url;   // 프로바이더 서버 end-point
-        string credential; //이메일, 핸드폰 등등 인증 방식, 여러개의 인증 방식이 있으니, 구조체 혹은 배열로 선언해줘야되나?
+        string credential_type; //이메일, 핸드폰 등등 인증 방식, 여러개의 인증 방식이 있으니, 구조체 혹은 배열로 선언해줘야되나?
     }
 
     struct CredentialResult
@@ -18,7 +18,7 @@ contract B_project {
         string enc_Partial_Key; // Provider pub_key로 암호화 한 partial_key
         string signed_data; // 데이터가 제대로 
         string user_pub_key; //유저의 퍼블릭 key? 이더리움 address를 의미하는건지?
-        string credential; //인증한 방식, payload.
+        string encrypted_payload; //인증한 방식, encrypted payload.
     }
 
     
@@ -31,10 +31,10 @@ contract B_project {
 
 
     //프로바이더가 자신을 프로바이더로 등록하는 함수, 웹앱에서만 호출 가능.
-    function registerProv(string memory ID, string memory public_key, string  memory name, string  memory base_url, string memory credential) public {
-        Providers_list.push(Provider(ID, public_key, name, base_url, credential)); //생성된 구조체 배열에 새로운 프로바이더 담기
+    function registerProv(string memory ID, string memory public_key, string  memory name, string  memory base_url, string memory credential_type) public {
+        Providers_list.push(Provider(ID, public_key, name, base_url, credential_type)); //생성된 구조체 배열에 새로운 프로바이더 담기
         //매핑도 추가해줘야 될듯
-        provider[ID] = Provider(ID, public_key, name, base_url, credential);
+        provider[ID] = Provider(ID, public_key, name, base_url, credential_type);
     }
 
     //시스템에 등록한 프로바이더 전체의 이름과 base_url을 return해주는 함수, 서버에서 콜, 
@@ -43,16 +43,16 @@ contract B_project {
         string[] memory provPubkey = new string[](Providers_list.length);
         string[] memory provname = new string[](Providers_list.length);
         string[] memory provbase_url = new string[](Providers_list.length);
-        string[] memory provcredential = new string[](Providers_list.length);
+        string[] memory provcredential_type = new string[](Providers_list.length);
                
         for(uint i = 0; i <= Providers_list.length; i++) {
              provID[i] = Providers_list[i].ID; //ID 배열
              provPubkey[i] = Providers_list[i].public_key; //pubkey 배열
              provname[i] = Providers_list[i].name; //이름 배열
              provbase_url[i] = Providers_list[i].base_url;
-             provcredential[i] = Providers_list[i].credential;
+             provcredential_type[i] = Providers_list[i].credential_type;
         }
-        return (provID, provPubkey, provname, provbase_url, provcredential);
+        return (provID, provPubkey, provname, provbase_url, provcredential_type);
     }
 
         //시스템에 등록한 프로바이더 전체의 이름과 base_url을 return해주는 함수, 서버에서 콜
@@ -61,8 +61,8 @@ contract B_project {
     }
 
    //유저의 부분키를 등록하는 함수
-    function registerPK(string memory ID, string memory public_key, string memory enc_partial_Key, string memory signed_data, string memory user_pub_key, string memory credential) public {
-        CredentialResult memory result = CredentialResult(ID, public_key, user_pub_key, enc_partial_Key, signed_data, credential);
+    function registerPK(string memory ID, string memory public_key, string memory enc_partial_Key, string memory signed_data, string memory user_pub_key, string memory encrypted_payload) public {
+        CredentialResult memory result = CredentialResult(ID, public_key, user_pub_key, enc_partial_Key, signed_data, encrypted_payload);
         //Credentialresult_list.push(result);
         credentialresults[user_pub_key].push(result);
 
@@ -74,14 +74,14 @@ contract B_project {
 
         string[] memory provID = new string[](Providers_list.length);
         string[] memory provPubkey = new string[](Providers_list.length);
-        string[] memory provcredential = new string[](Providers_list.length);
+        string[] memory provcredential_type = new string[](Providers_list.length);
 
         for(uint i = 0; i <= Providers_list.length; i++) {
             provID[i] = credentialresults[user_pub_key][i].Provider_ID;
             provPubkey[i] = credentialresults[user_pub_key][i].public_key;
-            provcredential[i] = credentialresults[user_pub_key][i].credential;
+            provcredential_type[i] = credentialresults[user_pub_key][i].encrypted_payload;
         }
-        return (provID, provPubkey, provcredential);
+        return (provID, provPubkey, provcredential_type);
     }
 
     //유저가 본인의 partial key를 맡긴 프로바이더의 목록을 return 해주는 함수 from provider 구조체
